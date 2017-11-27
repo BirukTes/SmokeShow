b9243cc54af3f8cb12089ae050ad9e0273321a54
Andrew Katsikas <apkatsikas@gmail.com>
2017-11-26 20:58:44 -0500

    boomerang frames

--- Assets/Scripts/Visual/AnimateTiledTexture.cs
+++ Assets/Scripts/Visual/AnimateTiledTexture.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
 public class AnimateTiledTexture : MonoBehaviour {
 
@@ -7,12 +9,42 @@ public class AnimateTiledTexture : MonoBehaviour {
     [SerializeField]
     float framesPerSecond;
     Material mat;
+    [SerializeField]
+    bool boomerang = false;
+    Texture[] boomerangFrames;
 
     void Start() {
         mat = GetComponent<Renderer>().material;
+        if ( boomerang ) {
+            boomerangFrames = (Texture[])frames.Clone();
+            Array.Reverse( boomerangFrames );
+            RemoveAt( ref boomerangFrames, 0);
+            RemoveAt( ref boomerangFrames, boomerangFrames.Length - 1 );
+            frames = ConcatArrays( frames, boomerangFrames );
+        }
     }
 
     void Update() {
-        mat.mainTexture = frames[(int)( Time.time * framesPerSecond ) % frames.Length];
+        var frame = (int)( Time.time * framesPerSecond ) % frames.Length;
+        mat.mainTexture = frames[frame];
+    }
+
+    public static void RemoveAt<T>( ref T[] arr, int index ) {
+        for ( int a = index; a < arr.Length - 1; a++ ) {
+            // moving elements downwards, to fill the gap at [index]
+            arr[a] = arr[a + 1];
+        }
+        // finally, let's decrement Array's size by one
+        Array.Resize( ref arr, arr.Length - 1 );
+    }
+
+    public static T[] ConcatArrays<T>( params T[][] list ) {
+        var result = new T[list.Sum( a => a.Length )];
+        int offset = 0;
+        for ( int x = 0; x < list.Length; x++ ) {
+            list[x].CopyTo( result, offset );
+            offset += list[x].Length;
+        }
+        return result;
     }
 }
