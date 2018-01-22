3bb23085beff9d2ccc23ee9b7920c3774efffb47
DESKTOP-6CT6E0O\vampi <vampirecomputerpeople@gmail.com>
2018-01-21 22:25:11 -0500

    fix reduce

--- Assets/Reduce.cs
+++ Assets/Reduce.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reduce : MonoBehaviour {
+public class Reduce : MonoBehaviour
+{
 
     private Material m_Material;    // Used to store material reference.
     private TriggerableElement element;
     [SerializeField]
-    private Color unityColor = new Color( 0, 0, 0, 0 );
+    private Color unityColor = new Color(0, 0, 0, 0);
     [SerializeField]
     private float reduceAmp = 1;
-	[SerializeField]
-	private float baseAlpha = .5f;
+    [SerializeField]
+    private float baseAlpha = .5f;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         element = GetComponent<TriggerableElement>();
         // Get reference to object's material.
         m_Material = GetComponent<Renderer>().material;
@@ -22,36 +24,43 @@ public class Reduce : MonoBehaviour {
         m_Material.color = unityColor;
     }
 
-    void Update() {
-        if ( element.state == States.ACTIVE ) {
-            float alpha = ( 1 - ( MessageBus.calcedAlpha * reduceAmp ) ) ;
-            if ( alpha < .1f) {
+    void Update()
+    {
+        if (element.state == States.ACTIVE)
+        {
+            float alpha = (baseAlpha - (MessageBus.calcedAlpha * reduceAmp));
+            if (alpha < .1f)
+            {
                 alpha = .1f;
             }
-			else if (alpha > .5f) {
-				alpha = baseAlpha;
-			}
-            m_Material.color = new Color( unityColor.r, unityColor.g, unityColor.b, alpha );
+            m_Material.color = new Color(unityColor.r, unityColor.g, unityColor.b, alpha);
         }
     }
 
-    public void IncrementSensitivityDivision() {
-        if ( reduceAmp == -.1f ) {
+    public void IncrementSensitivityDivision()
+    {
+        if (reduceAmp == -.1f)
+        {
             reduceAmp += .2f;
         }
-		else if (reduceAmp >=3) {
-			
-		}
-        else {
+        else if (reduceAmp >= 3)
+        {
+
+        }
+        else
+        {
             reduceAmp += .1f;
         }
     }
 
-    public void DecrementSensitivityDivision() {
-        if ( reduceAmp <= .1f ) {
-            //reduceAmp -= .2f;
+    public void DecrementSensitivityDivision()
+    {
+        if (reduceAmp <= .1f)
+        {
+            
         }
-        else {
+        else
+        {
             reduceAmp -= .1f;
         }
     }
